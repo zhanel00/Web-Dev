bf8f08406e14e2b1dd90750138503dc77caa0d8d
zhanel <zhanelstm@gmail.com>
2023-02-16 17:35:43 +0600

    Todo list

---
+++ Lab3/To-do list/js/script.js
@@ -0,0 +1,71 @@
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('.todo-input');
+const todoItems = document.querySelector('.todo-list');
+
+let todos = [];
+
+todoForm.addEventListener('submit',
+                        function(event) {
+                                event.preventDefault();
+                                addTodo(todoInput.value);
+                        });
+function addTodo(item) {
+    if (item != '') {
+        const todo = {
+            id : Date.now(),
+            name: item,
+            completed: false,
+        };
+        todos.push(todo)
+        renderTodos(todos);
+        todoInput.value = '';
+    }
+}
+
+function renderTodos(todos) {
+    todoItems.innerHTML = '';
+    todos.forEach(function(item) {
+        const checked = item.completed ? 'checked' : null;
+
+        const li = document.createElement('li');
+        li.setAttribute('class', 'item');
+        li.setAttribute('data-key', item.id);
+
+        if (item.completed) {
+            li.classList.add('checked');
+        }
+
+        li.innerHTML = `
+            <input type="checkbox" class="checkbox" ${checked}>
+            ${item.name}
+            <button class="delete-button">X</button>
+        `;
+        todoItems.append(li);
+    })
+}
+
+function toggle(id) {
+    todos.forEach(function(item) {
+        if (item.id == id) {
+            item.completed = !item.completed;
+        }
+    });
+}
+
+function deleteTodo(id) {
+    todos = todos.filter(function(item) {
+        return item.id != id;
+    });
+}
+
+todoItems.addEventListener('click', function(event) {
+    if (event.target.type === 'checkbox') {
+        toggle(event.target.parentElement.getAttribute('data-key'));
+    }
+
+    if (event.target.classList.contains('delete-button')) {
+        deleteTodo(event.target.parentElement.getAttribute('data-key'));
+    }
+    renderTodos(todos);
+})
+
