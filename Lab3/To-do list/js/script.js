f227f8d031a4d849dbc0a644513b50e87e60e7cc
zhanel <zhanelstm@gmail.com>
2023-02-16 17:56:57 +0600

    Todo list style modif

--- Lab3/To-do list/js/script.js
+++ Lab3/To-do list/js/script.js
@@ -38,7 +38,7 @@ function renderTodos(todos) {
         li.innerHTML = `
             <input type="checkbox" class="checkbox" ${checked}>
             ${item.name}
-            <button class="delete-button">X</button>
+            <button class="delete-button">x</button>
         `;
         todoItems.append(li);
     })
