17ec797719d3b0e6604d156c616580d3cec02e6a
zhanel00 <zhanelgmstm@gmail.com>
2023-04-05 21:53:01 +0600

    Lab8

--- Lab8/shop_back/api/models.py
+++ Lab8/shop_back/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
-    def str(self):
+    def __str__(self):
         return self.name
 
     def to_json(self):
@@ -18,7 +18,7 @@ class Product(models.Model):
     count = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
-    def str(self):
+    def __str__(self):
         return self.name
 
     def to_json(self):
